410b50ac05b9fa7765f6ddeda8810697ebf59bd4
Bart Devriendt <bart.devriendt@gmail.com>
2023-12-04 19:32:03 +0100

    Puzzle 4

---
+++ Models/ScratchCard.cs
@@ -0,0 +1,17 @@
+﻿namespace AOC2023.Models;
+
+public class ScratchCard
+{
+    public int Number { get; set; }
+    public List<int> DrawNumbers { get; set; }
+    public List<int> CardNumbers { get; set; }
+    public int Copies { get; set; }
+
+    public ScratchCard(int number, List<int> drawNumbers, List<int> cardNumbers)
+    {
+        Number = number;
+        DrawNumbers = drawNumbers;
+        CardNumbers = cardNumbers;
+        Copies = 1;
+    }
+}
---
+++ Puzzles/Puzzle4.cs
@@ -0,0 +1,65 @@
+﻿using AOC2023.Models;
+using Spectre.Console;
+
+namespace AOC2023.Puzzles;
+
+public class Puzzle4 : PuzzleBase
+{
+    private List<int> cardPoints = new();
+    private void ProcessCard(string line)
+    {
+        AnsiConsole.Console.WriteLine("Processing card " + line);
+        string[] parts = line.Substring(10).Split('|');
+        List<int> cardNumbers = new List<int>(parts[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => Int32.Parse(x)));
+        List<int> drawNumbers = new List<int>(parts[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => Int32.Parse(x)));
+        List<int> result = drawNumbers.FindAll(x => cardNumbers.Contains(x));
+        
+        cardPoints.Add((int)Math.Pow(2, result.Count - 1));
+
+    }
+    public override void Part1()
+    {
+        AnsiConsole.WriteLine("Puzzle 4 part 1");
+        AnsiConsole.WriteLine("Reading file");
+        ReadFileLineByLine("Data//puzzle4.txt", ProcessCard);
+        AnsiConsole.WriteLine("File read");
+        AnsiConsole.WriteLine("Sum is " + cardPoints.Sum());
+    }
+
+    public override void Part2()
+    {
+        AnsiConsole.WriteLine("Puzzle 4 part 2");
+        AnsiConsole.WriteLine("Reading file");
+        ReadFileLineByLine("Data//puzzle4.txt", ProcessCardPart2);
+        AnsiConsole.WriteLine("File read");
+        CountCopies();
+        AnsiConsole.WriteLine("Total scratch cards: " + scratchCards.Sum(x => x.Value.Copies));
+    }
+    
+    private Dictionary<int, ScratchCard> scratchCards = new();
+
+    private void CountCopies()
+    {
+        foreach(var cardNumber in scratchCards.Keys.OrderBy(x => x))
+        {
+            var card = scratchCards[cardNumber];
+            List<int> result = card.DrawNumbers.FindAll(x => card.CardNumbers.Contains(x));
+            int count = result.Count;
+            for (int j = cardNumber + 1; j < cardNumber + count + 1; j++)
+            {
+                scratchCards[j].Copies += card.Copies;
+            }
+            AnsiConsole.WriteLine("Card " + cardNumber + " has " + card.Copies + " copies");
+        }
+    }
+    
+    private void ProcessCardPart2(string line)
+    {
+        AnsiConsole.Console.WriteLine("Processing card " + line);
+        int CardId = Int32.Parse(line.Substring(5, 3).Trim());
+        string[] parts = line.Substring(10).Split('|');
+        List<int> cardNumbers = new List<int>(parts[0].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => Int32.Parse(x)));
+        List<int> drawNumbers = new List<int>(parts[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).ToList().Select(x => Int32.Parse(x)));
+        scratchCards[CardId] = new ScratchCard(CardId, drawNumbers, cardNumbers);
+    }
+}
