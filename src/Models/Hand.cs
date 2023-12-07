cdf09178acd9170df1be644faca1271ecd27b0ee
Bart Devriendt <bart.devriendt@gmail.com>
2023-12-07 18:48:49 +0100

    Day 7

---
+++ src/Models/Hand.cs
@@ -0,0 +1,188 @@
+﻿namespace AOC2023.Models;
+
+public enum HandType
+{
+    FiveOfAKind = 0,
+    FourOfAKind = 1,
+    FullHouse = 2,
+    ThreeOfAKind = 3,
+    TwoPair = 4,
+    OnePair = 5,
+    HighCard = 6
+}
+
+public class HandComparator : Comparer<Hand>
+{
+    public override int Compare(Hand? x, Hand? y)
+    {
+        int handXType = (int)x!.HandType;
+        int handYType = (int)y!.HandType;
+        if (handXType != handYType)
+            return handYType.CompareTo(handXType);
+        
+        int num = 0;
+        while(x.Cards[num] == y.Cards[num] && num < 4)
+        {
+            num++;
+        }
+        
+        return x.Cards[num].CompareTo(y.Cards[num]);
+    }
+}
+
+public class Hand
+{
+    public List<int> Cards { get; set; } = new List<int>();
+    public int Bid { get; set; }
+    
+    public HandType HandType { get; set; }
+
+    public Hand(string hand, int bid, bool jIsJoker = false)
+    {
+        for(int j = 0; j < hand.Length; j++)
+        {
+            Cards.Add(DetermineCardValue(hand[j], jIsJoker));
+        }
+
+        Bid = bid;
+        
+        if(jIsJoker)
+            DetermineHandTypeWithJoker();
+        else
+            DetermineHandType();
+    }
+
+    public void DetermineHandTypeWithJoker()
+    {
+        Dictionary<int, int> cardCounts = new Dictionary<int, int>();
+        foreach (var card in Cards)
+        {
+            if (cardCounts.ContainsKey(card))
+                cardCounts[card]++;
+            else
+                cardCounts.Add(card, 1);
+        }
+        
+        HandType = HandType.HighCard;
+
+        if (cardCounts.Values.Any(x => x == 5))
+        {
+            HandType = HandType.FiveOfAKind;
+
+        }
+        else if (cardCounts.Values.Any(x => x == 4))
+        {
+            HandType = HandType.FourOfAKind;
+
+        }
+        else if (cardCounts.Values.Any(x => x == 3) && cardCounts.Values.Any(x => x == 2))
+        {
+            HandType = HandType.FullHouse;
+
+        }
+        else if (cardCounts.Values.Any(x => x == 3))
+        {
+            HandType = HandType.ThreeOfAKind;
+
+        }
+        else if (cardCounts.Values.Count(x => x == 2) == 2)
+        {
+            HandType = HandType.TwoPair;
+
+        }
+        else if (cardCounts.Values.Any(x => x == 2))
+        {
+            HandType = HandType.OnePair;
+
+        }
+        
+        if (cardCounts.TryGetValue(1, out var count))
+        {
+            if (HandType == HandType.FourOfAKind) HandType = HandType.FiveOfAKind;
+            else if (HandType == HandType.FullHouse)
+            {
+                if (count == 1)
+                    HandType = HandType.FourOfAKind;
+                else
+                    HandType = HandType.FiveOfAKind;
+            }
+            else if(HandType == HandType.ThreeOfAKind) HandType = HandType.FourOfAKind;
+            else if(HandType == HandType.TwoPair)
+            {
+                if (count == 1)
+                    HandType = HandType.FullHouse;
+                else
+                    HandType = HandType.FourOfAKind;
+            }
+            else if(HandType == HandType.OnePair) HandType = HandType.ThreeOfAKind;
+            else if(HandType == HandType.HighCard) HandType = HandType.OnePair;
+        }
+    }
+    public void DetermineHandType()
+    {
+        Dictionary<int, int> cardCounts = new Dictionary<int, int>();
+        foreach (var card in Cards)
+        {
+            if (cardCounts.ContainsKey(card))
+                cardCounts[card]++;
+            else
+                cardCounts.Add(card, 1);
+        }
+
+        if (cardCounts.Values.Any(x => x == 5))
+        {
+            HandType = HandType.FiveOfAKind;
+            return;
+        }
+        if (cardCounts.Values.Any(x => x == 4))
+        {
+            HandType = HandType.FourOfAKind;
+            return;
+        }
+        if (cardCounts.Values.Any(x => x == 3) && cardCounts.Values.Any(x => x == 2))
+        {
+            HandType = HandType.FullHouse;
+            return;
+        }
+        if (cardCounts.Values.Any(x => x == 3))
+        {
+            HandType = HandType.ThreeOfAKind;
+            return;
+        }
+        if (cardCounts.Values.Count(x => x == 2) == 2)
+        {
+            HandType = HandType.TwoPair;
+            return;
+        }
+        if (cardCounts.Values.Any(x => x == 2))
+        {
+            HandType = HandType.OnePair;
+            return;
+        }
+        HandType = HandType.HighCard;
+        
+    }
+    private int DetermineCardValue(char card, bool jIsJoker = false)
+    {
+        switch (card)
+        {
+            case 'A':
+                return 14;
+            case 'K':
+                return 13;
+            case 'Q':
+                return 12;
+            case 'J':
+                return  jIsJoker ? 1 : 11;
+            case 'T':
+                return 10;
+            default:
+                return Int32.Parse(card.ToString());
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{Cards[0]} {Cards[1]} {Cards[2]} {Cards[3]} {Cards[4]} - {HandType}";
+    }
+}
---
+++ src/Puzzles/Puzzle7.cs
@@ -0,0 +1,77 @@
+﻿using AOC2023.Models;
+using Spectre.Console;
+
+namespace AOC2023.Puzzles;
+
+public class Puzzle7 : PuzzleBase
+{
+    private List<Hand> hands = new List<Hand>();
+    private bool jIsJoker = false;
+    private void ProcessLine(string line)
+    {
+        string[] parts = line.Split(' ');
+        hands.Add(new Hand(parts[0], int.Parse(parts[1]), jIsJoker));
+    }
+    
+    public override void Part1()
+    {
+        AnsiConsole.WriteLine("Puzzle 7 part 1");
+        AnsiConsole.WriteLine("Reading file");
+        ReadFileLineByLine("Data//puzzle7.txt", ProcessLine);
+        AnsiConsole.WriteLine("File read");
+
+        try
+        {
+            hands.Sort(new HandComparator());
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.WriteException(e);
+            throw;
+        }
+        
+
+        int bidTotal = 0;
+        for (var index = 0; index < hands.Count; index++)
+        {
+            var hand = hands[index];
+            bidTotal += hand.Bid * (index + 1);
+        }
+        
+        AnsiConsole.WriteLine("Bid total is " + bidTotal);
+    }
+
+    public override void Part2()
+    {
+        jIsJoker = true;
+        AnsiConsole.WriteLine("Puzzle 7 part 2");
+        AnsiConsole.WriteLine("Reading file");
+        ReadFileLineByLine("Data//puzzle7.txt", ProcessLine);
+        AnsiConsole.WriteLine("File read");
+
+        foreach (var hand in hands)
+        {
+            AnsiConsole.WriteLine(hand.ToString());
+        }
+
+        try
+        {
+            hands.Sort(new HandComparator());
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.WriteException(e);
+            throw;
+        }
+        
+
+        int bidTotal = 0;
+        for (var index = 0; index < hands.Count; index++)
+        {
+            var hand = hands[index];
+            bidTotal += hand.Bid * (index + 1);
+        }
+        
+        AnsiConsole.WriteLine("Bid total is " + bidTotal);
+    }
+}
